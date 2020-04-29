a47c985907b936ea11a2bd0d69d0aec530e62cd4
Douglas Creager <dcreager@dcreager.net>
2020-04-29 08:45:49 -0400

    Parse rational numbers from path notation

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,45 @@
 // express or implied.  See the License for the specific language governing permissions and
 // limitations under the License.
 // ------------------------------------------------------------------------------------------------
+
+use num_rational::Ratio;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct PathIdentifier(Ratio<u64>);
+
+impl std::str::FromStr for PathIdentifier {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut ratio = Ratio::from_integer(0);
+        for piece in s.split('.').rev() {
+            let piece: u64 = piece.parse()?;
+            ratio = (ratio + piece).recip();
+        }
+        Ok(PathIdentifier(ratio.recip()))
+    }
+}
+
+impl PartialEq<(u64, u64)> for PathIdentifier {
+    fn eq(&self, other: &(u64, u64)) -> bool {
+        self.0 == Ratio::from(*other)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn parse_id(s: &str) -> PathIdentifier {
+        s.parse().expect("Cannot parse identifier")
+    }
+
+    #[test]
+    fn can_parse_paths() {
+        assert_eq!(parse_id("3"), (3, 1));
+        assert_eq!(parse_id("3.12"), (37, 12));
+        assert_eq!(parse_id("3.12.5"), (188, 61));
+        assert_eq!(parse_id("3.12.5.1"), (225, 73));
+        assert_eq!(parse_id("3.12.5.1.21"), (4913, 1594));
+    }
+}
